956125eebd303e8aff52309e8ea7b10b4975b1ea
rupikz <nikolau22233@gmail.com>
2021-01-14 17:03:28 +0300

    fix environment

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,14 +2,15 @@ import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
   type: 'mongodb',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOSTNAME || 'localhost',
   port: +process.env.DB_PORT || 27017,
-  username: process.env.DB_USER || '',
-  password: process.env.PASSWORD || '',
+  username: process.env.DB_USERNAME || '',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'test',
   synchronize: true,
   dropSchema: false,
   useUnifiedTopology: true,
+  connectTimeoutMS: 10000,
   logging: false,
   entities: [`src/entity/*.entity.ts`],
   migrations: [`src/migration/*.ts`],
--- src/config/config.ts
+++ src/config/config.ts
@@ -3,5 +3,5 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  PORT: 3000
+  PORT: process.env.PORT || 3000
 };
